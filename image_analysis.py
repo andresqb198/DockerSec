bc35a11de4eb00538b7b272d4bf173e735336e69
andresqb198 <andresqb198@gmail.com>
2024-10-31 15:41:14 -0500

    ✨ [ADD] Search for URL

--- image_analysis.py
+++ image_analysis.py
@@ -1,6 +1,7 @@
 import subprocess
 import json
 import os
+import re
 import shutil
 
 
@@ -11,10 +12,22 @@ def inspect_image(image):
 
     container_id = subprocess.check_output(["docker", "create", image]).decode().strip()
 
-    check_git_command = ["docker", "cp", f"{container_id}:/", "/tmp/docker_inspect"]
-    subprocess.run(check_git_command, capture_output=True)
+    check_git_command = ["docker", "run", f"{image}", "ls", "-a"]
+    result_git = subprocess.run(check_git_command, capture_output=True)
 
-    git_folder_exists = os.path.exists("/tmp/docker_inspect/.git")
+    if b'.git' in result_git.stdout.split():
+        git_folder_exists = True
+        get_git_info_command = ["docker", "run", f"{image}", "cat", ".git/config"]
+        git_info = subprocess.run(get_git_info_command, capture_output=True, text=True)
+        git_info = git_info.stdout.strip()
+        match = re.search(r'url\s*=\s*(.+)', git_info)
+        if match:
+            repo_url = match.group(1).strip()
+            print("La URL del repositorio es:", repo_url)
+        else:
+            print("No se encontró la URL del repositorio.")
+    else:
+        git_folder_exists = False
     
     base_python_version = None
     python_check_command = ["docker", "run", "--rm", image, "python3", "--version"]
@@ -31,6 +44,7 @@ def inspect_image(image):
         "image": image,
         "has_git_folder": git_folder_exists,
         "base_python_version": base_python_version,
+        "repo_url": repo_url if git_folder_exists else None,
         "is_python_version_below_3.8": (
             base_python_version < "3.8" if base_python_version != "Unknown or Python not installed" else False
         )
